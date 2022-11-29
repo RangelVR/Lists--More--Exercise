11fd2f15816a78cf011599f3ec0f10ac87ec6e11
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-11-29 23:54:57 +0200

    Add files via upload

---
+++ 02. Car Race/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Car_Race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] raceTrack = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            double leftTime = 0;
+            double rightTime = 0;
+            int finish = raceTrack.Length / 2;
+
+            for (int left = 0; left < finish; left++)
+            {
+                if (raceTrack[left] == 0)
+                {
+                    leftTime *= 0.8;
+                }
+                leftTime += raceTrack[left];
+            }
+
+            for (int right = raceTrack.Length - 1; right > finish; right--)
+            {
+                if (raceTrack[right] == 0)
+                {
+                    rightTime *= 0.8;
+                }
+                rightTime += raceTrack[right];
+            }
+
+            if (leftTime < rightTime)
+            {
+                Console.WriteLine($"The winner is left with total time: {Math.Round(leftTime,1)}");
+            }
+            else
+            {
+                Console.WriteLine($"The winner is right with total time: {rightTime}");
+            }
+
+        }
+    }
+}
