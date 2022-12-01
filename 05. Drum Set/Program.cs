8c7e55add398f9520956e91a1b0451271b5f653f
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-12-01 23:06:45 +0200

    Add files via upload

---
+++ 05. Drum Set/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Drum_Set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double savings = double.Parse(Console.ReadLine());
+            List<int> drumSet = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> referenceList = new List<int> (drumSet);
+            string command;
+
+            while ((command = Console.ReadLine()) != "Hit it again, Gabsy!")
+            {
+                int power = int.Parse(command);
+                for (int i = 0; i < drumSet.Count; i++)
+                {
+                    drumSet[i] -= power;
+
+                    if (drumSet[i] <= 0)
+                    {
+                        if (savings - referenceList[i] * 3 >= 0)
+                        {
+                            drumSet[i] = referenceList[i];
+                            savings -= referenceList[i] * 3;
+                        }
+                        else
+                        {
+                            drumSet.RemoveAt(i);
+                            referenceList.RemoveAt(i);
+                            if (i != drumSet.Count)
+                            {
+                                i--;
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ", drumSet));
+            Console.WriteLine($"Gabsy has {savings:f2}lv.");
+        }
+    }
+}
