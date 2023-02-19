b4380cefd8de0944d46ab5f089b301d9e9931b61
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-02-19 21:21:23 +0200

    Update Program.cs

--- 01. Messaging/Program.cs
+++ 01. Messaging/Program.cs
@@ -28,3 +28,34 @@ for (int i = 0; i < numbers.Length; i++)
 }
 
 Console.WriteLine(message);
+
+-----------------------------------------------------------------------
+//using System.Reflection;
+
+//int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+//List<char> text = Console.ReadLine().ToCharArray().ToList();
+
+//string message = string.Empty;
+
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//	int currNum = numbers[i];
+//    int index = 0;
+
+//    while (currNum > 0)
+//	{
+//		index += currNum % 10;
+//		currNum /= 10;
+//	}
+
+//	if (index >= text.Count)
+//	{
+//		index %= text.Count;
+//	}
+
+//	message += text[index];
+//	text.RemoveAt(index);
+//}
+
+//Console.WriteLine(message);	
