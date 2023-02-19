8ca662b2e4cf0d0f693f425965dde0a953a22685
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-02-19 21:22:29 +0200

    Update Program.cs

--- 01. Messaging/Program.cs
+++ 01. Messaging/Program.cs
@@ -41,9 +41,9 @@ Console.WriteLine(message);
 //for (int i = 0; i < numbers.Length; i++)
 //{
 //	int currNum = numbers[i];
-//    int index = 0;
+//      int index = 0;
 
-//    while (currNum > 0)
+//      while (currNum > 0)
 //	{
 //		index += currNum % 10;
 //		currNum /= 10;
