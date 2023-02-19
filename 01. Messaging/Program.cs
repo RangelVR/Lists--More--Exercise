cd98d87b7a3ce9001bb24e4cd5095bf9e21ef87e
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-02-19 21:08:42 +0200

    Update Program.cs

--- 01. Messaging/Program.cs
+++ 01. Messaging/Program.cs
@@ -1,37 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Reflection;
 
-namespace _01._Messaging
+int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+string text = Console.ReadLine();
+
+int counter = 0;
+string message = string.Empty;
+
+for (int i = 0; i < numbers.Length; i++)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            List<string> nums = Console.ReadLine().Split().ToList();
-            List<char> text = Console.ReadLine().ToCharArray().ToList();
-            string word = string.Empty;
+	int currNum = numbers[i];
+    int index = 0;
 
-            for (int i = 0; i < nums.Count; i++)
-            {
-                string @string = nums[i];
-                int sum = 0;
+    while (currNum > 0)
+	{
+		index += currNum % 10;
+		currNum /= 10;
+	}
 
-                for (int k = 0; k < @string.Length; k++)
-                {
-                    int currNumber = @string[k] - 48;
-                    sum += currNumber;
-                }
-                int index = sum;
+	if (index >= text.Length)
+	{
+		index %= text.Length;
+	}
 
-                if (index >= text.Count)
-                {
-                    index -= text.Count;
-                }
-                word += text[index];
-                text.RemoveAt(index);
-            }
-            Console.WriteLine(word);
-        }
-    }
+	message += text[index + counter];
+	counter++;
 }
+
+Console.WriteLine(message);
